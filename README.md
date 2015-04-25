8118ab51c3b2e76000d0a41f643fd27206910a19
Sandro Simas <sandro.csimas@gmail.com>
2015-04-25 00:59:12 -0300

    Improving validations and creating more tests; Writing README.md;

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# bookshelf-filteration
-This plugin filter attributes when saves or updates an object. After that uses validate.js to validate remaining attributes.
+# Bookshelf Filteration (Filter + Validation)
+Sometime you don't want to save all attributes of a model. There are cases that you want to save specific attributes according to scenario, for example an user update, an user creation, or when you want to force a avatar only edition.
+
+Bookshelf-filteration also uses validate.js to validate your model attributes with the difference that only filtered attributes will be validated in the case of filter presence.
+
+# Filtering attributes example:
+````
+var User = Bookshelf.Model.extend({
+  idAttribute: 'id',
+  tableName: 'user',
+  filters: {
+    // Insert scenario (used when a model will be inserted)
+    insert: [{name: 'first_name', required: true}, {name: 'email', required: true}, {name: 'password', required: true}, 'last_name'],
+    // Update scenario (used when a model will be updated)
+    update: ['first_name', 'last_name', 'email', 'password', 'phone'],
+    // Custom scenario (used when passes the option scenario)
+    changeAvatar: ['avatar']
+  }
+});
+````
+
+# Validating attributes example:
+````
+var User = Bookshelf.Model.extend({
+  idAttribute: 'id',
+  tableName: 'user',
+  // With no filters all attributes will be validated, independent of scenatio.
+  validations: {
+    first_name: {presence: true, length: {minimum: 3}},
+    last_name: {length: {minimum: 3}},
+    email: {presence: true, email: true},
+    password: {presence: true, length: {minimum: 3}},
+    phone: {format: /\+\d{2} \d{2} \d{4,5}\-\d{4}/}
+  }
+});
+````
+
+# Combining filters and validations example:
+````
+var User = Bookshelf.Model.extend({
+  idAttribute: 'id',
+  tableName: 'user',
+  validations: {
+    first_name: {presence: true, length: {minimum: 3}},
+    last_name: {length: {minimum: 3}},
+    email: {presence: true, email: true},
+    password: {presence: true, length: {minimum: 3}},
+    phone: {format: /\+\d{2} \d{2} \d{4,5}\-\d{4}/}
+  },
+  filters: {
+    insert: [{name: 'first_name', required: true}, {name: 'email', required: true}, {name: 'password', required: true}, 'last_name'],
+    update: ['first_name', 'last_name', 'email', 'password', 'phone'],
+    changeAvatar: ['avatar']
+  }
+});
+````
+
+# Why the required feature if validate.js has the presence validation?
+
+The presence validation considers that null, blank are valid, regardless of others attribute validations. The problem is that sometimes you want to accept null values but not blanks.
+Example using validate.js:
+````
+validate({first_name: 'Sandro', last_name: 'Simas'}, {length: {minimum: 3}});
+// [] OK, last_name in user table will be updated to 'Simas'
+````
+````
+validate({first_name: 'Sandro', last_name: null}, {length: {minimum: 3}});
+// [] OK, last_name in user table will be updated to null
+````
+validate({first_name: 'Sandro', last_name: ''}, {length: {minimum: 3}});
+// [] Not OK because we expect a validation error since 0 length is less than 3
+````
+
--- test/tests.js
+++ test/tests.js
@@ -19,6 +19,28 @@ var User = Bookshelf.Model.extend({
   }
 });
 
+var UserWithNoFilters = Bookshelf.Model.extend({
+  idAttribute: 'id',
+  tableName: 'user',
+  validations: {
+    first_name: {presence: true, length: {minimum: 3}},
+    last_name: {presence: true, length: {minimum: 3}},
+    email: {presence: true, email: true},
+    password: {presence: true, length: {minimum: 3}},
+    phone: {presence: true, format: /\+\d{2} \d{2} \d{4,5}\-\d{4}/}
+  }
+});
+
+var UserWithNoValidations = Bookshelf.Model.extend({
+  idAttribute: 'id',
+  tableName: 'user',
+  filters: {
+    insert: [{name: 'first_name', required: true}, {name: 'email', required: true}, {name: 'password', required: true}, 'last_name'],
+    update: ['first_name', 'last_name', 'email', 'password', 'phone'],
+    changeAvatar: ['avatar']
+  }
+});
+
 describe('validations', function() {
   before(function() {
     return Bookshelf.knex.schema.dropTableIfExists('user').then(function() {
@@ -36,41 +58,12 @@ describe('validations', function() {
     });
   });
 
-  it('should fail when passes an empty object', function() {
-    return User.forge({}).save().then(function() {
-      throw new Error('User should not be saved');
-    }).catch(ValidationError, function(err) {
-      expect(err.errors).to.have.length(3);
-      expect(err.errors[0]).to.deep.equal({type: 'required', attribute: 'first_name', errors: ['Attribute first_name is required']});
-      expect(err.errors[1]).to.deep.equal({type: 'required', attribute: 'email', errors: ['Attribute email is required']});
-      expect(err.errors[2]).to.deep.equal({type: 'required', attribute: 'password', errors: ['Attribute password is required']});
-    });
-  });
-
-  it('should fail when required attributes is not present', function() {
-    return User.forge({first_name: 'Sandro', email: 'sandro.csimas@gmail.com'}).save().then(function() {
-      throw new Error('User should not be saved');
-    }).catch(ValidationError, function(err) {
-      expect(err.errors).to.have.length(1);
-      expect(err.errors[0]).to.deep.equal({type: 'required', attribute: 'password', errors: ['Attribute password is required']});
-    });
-  });
-
   it('should filter attributes on save', function() {
     return User.forge({first_name: 'Sandro', email: 'sandro.csimas@gmail.com', password: '123456', phone: '3333-3333'}).save().then(function(user) {
       expect(user.get('first_name')).to.equal('Sandro');
       expect(user.get('email')).to.equal('sandro.csimas@gmail.com');
       expect(user.get('password')).to.equal('123456');
       expect(user.get('phone')).to.be.undefined;
-    });
-  });
-
-  it('should fail when attributes is empty after filtering attributes on update', function() {
-    return User.forge({first_name: 'Sandro', email: 'sandro.csimas@gmail.com', password: '123456'}).save().then(function(user) {
-      return User.forge({id: user.id, registration_date: new Date(2010, 4, 4)}).save();
-    }).catch(ValidationError, function(err) {
-      expect(err.errors).to.have.length(1);
-      expect(err.errors[0]).to.deep.equal({type: 'update.filter.empty', errors: ['No attributes to update after filtering']});
     });
   });
 
@@ -105,12 +98,71 @@ describe('validations', function() {
     });
   });
 
+  it('should validate model without filtering attributes because model has no filters', function() {
+    return UserWithNoFilters.forge({first_name: 'Sandro', last_name: 'Simas', email: 'sandro.csimas@gmail.com', password: '123456', phone: '+55 71 3333-3333'}).save().then(function(user) {
+      expect(user.get('first_name')).to.equal('Sandro');
+      expect(user.get('last_name')).to.equal('Simas');
+      expect(user.get('email')).to.equal('sandro.csimas@gmail.com');
+      expect(user.get('password')).to.equal('123456');
+      expect(user.get('phone')).to.equal('+55 71 3333-3333');
+    });
+  });
+
+  it('should fail with required fields errors when passes an empty object', function() {
+    return User.forge({}).save().then(function() {
+      throw new Error('User should not be saved');
+    }).catch(ValidationError, function(err) {
+      expect(err.errors).to.have.length(3);
+      expect(err.errors[0]).to.deep.equal({type: 'required', attribute: 'first_name', errors: ['Attribute first_name is required']});
+      expect(err.errors[1]).to.deep.equal({type: 'required', attribute: 'email', errors: ['Attribute email is required']});
+      expect(err.errors[2]).to.deep.equal({type: 'required', attribute: 'password', errors: ['Attribute password is required']});
+    });
+  });
+
+  it('should fail with required field error when attribute is not present', function() {
+    return User.forge({first_name: 'Sandro', email: 'sandro.csimas@gmail.com'}).save().then(function() {
+      throw new Error('User should not be saved');
+    }).catch(ValidationError, function(err) {
+      expect(err.errors).to.have.length(1);
+      expect(err.errors[0]).to.deep.equal({type: 'required', attribute: 'password', errors: ['Attribute password is required']});
+    });
+  });
+
+  it('should fail when attribute is invalid', function() {
+    return User.forge({first_name: 'Sandro', last_name: 'DJ', email: 'sandro.csimas@gmail.com', password: '12'}).save().then(function() {
+      throw new Error('User should not be saved');
+    }).catch(ValidationError, function(err) {
+      expect(err.errors).to.have.length(2);
+      expect(err.errors[0]).to.deep.equal({type: 'invalid', attribute: 'password', errors: ['Password is too short (minimum is 3 characters)']});
+      expect(err.errors[1]).to.deep.equal({type: 'invalid', attribute: 'last_name', errors: ['Last name is too short (minimum is 3 characters)']});
+    });
+  });
+
+  it('should fail when attributes is empty after filtering attributes on update', function() {
+    return User.forge({first_name: 'Sandro', email: 'sandro.csimas@gmail.com', password: '123456'}).save().then(function(user) {
+      return User.forge({id: user.id, registration_date: new Date(2010, 4, 4)}).save();
+    }).catch(ValidationError, function(err) {
+      expect(err.errors).to.have.length(1);
+      expect(err.errors[0]).to.deep.equal({type: 'filter.empty', errors: ['No attributes remaining after filtering']});
+    });
+  });
+
   it('should fail when passes an invalid filter scenario', function() {
     return User.forge({first_name: 'Sandro', email: 'sandro.csimas@gmail.com', password: '123456'}).save().then(function(user) {
       return User.forge({id: user.id, registration_date: new Date(2010, 4, 4)}).save(null, {scenario: 'custom'});
     }).catch(ValidationError, function(err) {
       expect(err.errors).to.have.length(1);
-      expect(err.errors[0]).to.deep.equal({type: 'scenario.notFound', errors: ['Scenario with name custom does not exist']});
+      expect(err.errors[0]).to.deep.equal({type: 'scenario.notfound', errors: ['Scenario with name custom does not exist']});
     });
   });
+
+  it('should fail when model has no filters and fields are not valid', function() {
+    return UserWithNoFilters.forge({first_name: 'Sandro', email: 'sandro.csimas@gmail.com', password: '123456', phone: '+55 71 3333-3333'}).save().then(function(user) {
+      throw new Error('User should not be saved');
+    }).catch(ValidationError, function(err) {
+      expect(err.errors).to.have.length(1);
+      expect(err.errors[0]).to.deep.equal({type: 'invalid', attribute: 'last_name', errors: ['Last name can\'t be blank']});
+    });
+  });
+
 });
